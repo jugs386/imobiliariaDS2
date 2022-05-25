72ae7ecc03164db336e2fe684ae02a2e8ab4284c
Juliana de Sá <julianagsa@yahoo.com.br>
2022-05-25 19:30:23 -0300

    Usuario controller - salvar

---
+++ controller/UsuarioController.php
@@ -0,0 +1,18 @@
+<?php
+require_once '../model/Usuario.php';
+
+class UsuarioController{
+
+    public function salvar(){
+
+        $usuario = new Usuario();
+
+        $usuario->setLogin($_POST['login']);
+        $usuario->setSenha($_POST['senha1']);
+        $usuario->setPermissao($_POST['permissao']);
+
+        $usuario->save();
+    }
+}
+
+?>
