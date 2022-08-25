472a113f70f6f8bcbb12de3cd3d0dce9c73bef75
Juliana de Sá <julianagsa@yahoo.com.br>
2022-08-25 19:47:59 -0300

    controller

--- controller/ImovelController.php
+++ controller/ImovelController.php
@@ -3,10 +3,26 @@ require_once 'model/Imovel.php';
 
 class ImovelController{
 
-    public static function salvar(){
+    public static function salvar($fotoAtual = '', $fotoTipo=''){
 
         $imovel = new Imovel();
 
+        $imagem = array();
+        if(is_uploaded_file($_FILES['foto']['tmp_name'])){
+            $imagem['data'] = file_get_contents($_FILES['foto']['tmp_name']);
+            $imagem['tipo'] = $_FILES['foto']['type'];
+
+        }
+
+        if(!empty($imagem)){
+            $imovel->setFoto($imagem['data']);
+            $imovel->setFotoTipo($imagem['tipo']);
+        }else{
+            $imovel->setFoto($fotoAtual);
+            $imovel->setFotoTipo($fotoTipo);
+        }
+
+        $imovel->setId($_POST['id']);
         $imovel->setDescricao($_POST['descricao']);
         $imovel->setValor($_POST['valor']);
         $imovel->setTipo($_POST['tipo']);
